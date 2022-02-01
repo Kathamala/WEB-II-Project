b3a03e4f33176751c971902d8d75e10a94fd4e83
Sandino <37051758+paulosandinof@users.noreply.github.com>
2022-01-31 22:06:33 -0300

    remove node_modules and dist folder from commits

--- dist/users/entities/user.entity.d.ts
+++
@@ -1,9 +0,0 @@
-import { Document } from "mongoose";
-export declare type UserDocument = User & Document;
-export declare class User {
-    id: string;
-    username: string;
-    email: string;
-    password: string;
-}
-export declare const UserSchema: import("mongoose").Schema<Document<User, any, any>, import("mongoose").Model<Document<User, any, any>, any, any, any>, any>;
