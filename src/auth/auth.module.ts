8df913ab117341c9fbb64e4dbc874705f27a2a8d
Thiago Vinícius <thiagovao2000@hotmail.com>
2022-02-07 18:05:24 -0300

    Sets login expiration time to 5 minutes

--- src/auth/auth.module.ts
+++ src/auth/auth.module.ts
@@ -14,7 +14,7 @@ import { AuthController } from './controllers/auth.controller';
     PassportModule,
     JwtModule.register({
       secret: jwtConstants.secret,
-      signOptions: { expiresIn: '60s' },
+      signOptions: { expiresIn: '300s' },
     }),
   ],
   providers: [AuthService, LocalStrategy, JwtStrategy],
